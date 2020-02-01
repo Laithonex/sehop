ec74dace51f6e0fd193e2a3b2512a33bdce0dc73
Laithonex <laith.aljufaili966@gmail.com>
2020-02-01 20:16:42 +0400

    strip payment done

---
+++ client/src/Component/strip-button/strip-button.component.jsx
@@ -0,0 +1,88 @@
+// import React from 'react'; 
+// import StripeCheckout from 'react-stripe-checkout'; 
+// import axios from 'axios'; 
+
+// const StripeCheckoutButton = ({price}) => {
+//     const priceForStripe = price * 100; 
+//     const publishableKey = 'pk_test_w13L4KeuozxaeDcTpckYqGDG00NmyWQd3B';
+    
+//     const onToken = token => {
+//          axios({
+//              url: 'payment',
+//              method: 'post',
+//              data: {
+//                  amount: priceForStripe,
+//                  token: token
+//              }
+//          }).then(response  => {
+//              alert('succesful payment'); 
+//          }).catch(error => {
+//              console.log('Payment Error: ', error)
+//              alert('there was an issue with your payment. Plase make sure that you use the provided credit cart');
+//          });
+//     };
+
+//     return(
+//         <StripeCheckout
+//             label='Pay Now' 
+//             name='eshop'
+//             billingAddress
+//             shippingAddress
+//             image='https://svgshare.com/i/CUz.svg'
+//             description={`Your total is $${price}`}
+//             amount={priceForStripe}
+//             panelLabel='Pay now'
+//             token={onToken}
+//             stripeKey={publishableKey}
+
+//         />
+//     );
+// };
+
+// export default StripeCheckoutButton;
+
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey = 'pk_test_w13L4KeuozxaeDcTpckYqGDG00NmyWQd3B';
+
+  const onToken = token => {
+    axios({
+      url: 'payment',
+      method: 'post',
+      data: {
+        amount: priceForStripe,
+        token: token
+      }
+    })
+      .then(response => {
+        alert('succesful payment');
+      })
+      .catch(error => {
+        console.log('Payment Error: ', error);
+        alert(
+          'There was an issue with your payment! Please make sure you use the provided credit card.'
+        );
+      });
+  };
+
+  return (
+    <StripeCheckout
+      label='Pay Now'
+      name='CRWN Clothing Ltd.'
+    //   billingAddress
+    //   shippingAddress
+      image='https://svgshare.com/i/CUz.svg'
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel='Pay Now'
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
